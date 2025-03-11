3c289b5d5cd995adccbcd5c9d027b992c942427a
Olivia Dondas <olivia.dondas@laplateforme.io>
2025-03-11 14:04:09 +0100

    jour01 job04

--- jour01/job04/script.js
+++ jour01/job04/script.js
@@ -1,8 +1,12 @@
-function bisextile() {
-  let annee = document.getElementById("annee").value;
-  if (annee % 4 == 0 && (annee % 100 != 0 || annee % 400 == 0)) {
+function bisextile(année) {
+  if ((année % 4 === 0 && année % 100 !== 0) || année % 400 === 0) {
     return true;
   } else {
     return false;
   }
 }
+
+console.log("L'année 2000 est bissextile ?", bisextile(2000));
+console.log("L'année 2020 est bissextile ?", bisextile(2020));
+console.log("L'année 2021 est bissextile ?", bisextile(2021));
+console.log("L'année 1900 est bissextile ?", bisextile(1900));
